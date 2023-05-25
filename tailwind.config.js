51ea529354a09241a82fe41d44530a7f3bb24005
hhuyen1234 <82390604+hhuyen1234@users.noreply.github.com>
2023-05-25 08:29:25 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      flex: {
+        '2':'2 2 0%'
+
+      }
+
+    }
+    }
+    
+  }
+
